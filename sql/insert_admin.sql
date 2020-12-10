e9dded020300fe4d435f4cb9b22cf05d4f12feaa
beresgabor76 <beres.gabor.76@gmail.com>
2020-12-10 19:08:21 +0100

    css módosítás

---
+++ sql/insert_admin.sql
@@ -0,0 +1,6 @@
+USE Konyvtar;
+
+INSERT INTO Konyvtarosok
+VALUES ('admin', SHA('admin'), 'Béres', 'Gábor', '', '', 'Jászberény', '1976-02-18', 'Farkas','Julianna', 
+5100, 'Jászberény', 'Futrinka utca', '28.', '06201212121', 'beres.gabor@gmail.com', TRUE);
+
