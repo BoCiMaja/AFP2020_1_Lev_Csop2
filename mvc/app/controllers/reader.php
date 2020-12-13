d55378d79469fc2e2e1603cd5f1b35ccb950d90c
beresgabor76 <beres.gabor.76@gmail.com>
2020-12-13 22:28:14 +0100

    ReaderModel módosítás

--- mvc/app/controllers/reader.php
+++ mvc/app/controllers/reader.php
@@ -14,19 +14,51 @@ class Reader extends Controller {
         if (array_key_exists('submit', $_POST))
         {
             $readerModel = $this->model('ReaderModel');
+            $felhasznaloi_nev = trim($_POST['felhasznaloi_nev']); 
+            $jelszo = trim($_POST['jelszo']);
+            $csaladi_nev = trim($_POST['csaladi_nev']);
+            $utonev = trim($_POST['utonev']);
+            $szuletesi_csaladi_nev = trim($_POST['szuletesi_csaladi_nev']);
+            $szuletesi_utonev = trim($_POST['szuletesi_utonev']);
+            $anyja_szuletesi_csaladi_neve = trim($_POST['anyja_szuletesi_csaladi_neve']);
+            $anyja_szuletesi_utoneve = trim($_POST['anyja_szuletesi_utoneve']);
+            $szuletesi_hely = trim($_POST['szuletesi_hely']);
+            $szuletesi_datum = trim($_POST['szuletesi_datum']);
+            $lakcim_iranyitoszam = trim($_POST['lakcim_iranyitoszam']);
+            $lakcim_varos = trim($_POST['lakcim_varos']);
+            $lakcim_utca = trim($_POST['lakcim_utca']);
+            $lakcim_hazszam = trim($_POST['lakcim_hazszam']);
+            $telefonszam = trim($_POST['telefonszam']);
+            $email = trim($_POST['email']);            
+            $olvasojegy_azonosito = trim($_POST['olvasojegy_azonosito']);
             
             $error = $readerModel->validateReaderData1(
-                        $_POST['olvasojegy_azonosito'], $_POST['felhasznaloi_nev'], $_POST['jelszo'],
-                        $_POST['szuletesi_csaladi_nev'], $_POST['szuletesi_utonev'], 
-                        $_POST['anyja_szuletesi_csaladi_neve'], $_POST['anyja_szuletesi_utoneve'],
-                        $_POST['szuletesi_hely'], $_POST['szuletesi_datum']);
+                        $olvasojegy_azonosito, $felhasznaloi_nev, $jelszo,
+                        $szuletesi_csaladi_nev, $szuletesi_utonev, 
+                        $anyja_szuletesi_csaladi_neve, $anyja_szuletesi_utoneve,
+                        $szuletesi_hely, $szuletesi_datum);
             
-            $error .= $readerModel->validateReaderData2($_POST['csaladi_nev'], $_POST['utonev'], 
-                        $_POST['lakcim_iranyitoszam'], $_POST['lakcim_varos'], $_POST['lakcim_utca'], $_POST['lakcim_hazszam'],
-                        $_POST['telefonszam'], $_POST['email']);
+            $error .= $readerModel->validateReaderData2($csaladi_nev, $utonev, 
+                        $lakcim_iranyitoszam, $lakcim_varos, $lakcim_utca, $lakcim_hazszam,
+                        $telefonszam, $email);
             
             if (!$error)
-                $error = $readerModel->registerReader($_POST);
+                $error = $readerModel->registerReader(  $felhasznaloi_nev,                                                   $jelszo,
+                                                        $csaladi_nev,
+                                                        $utonev,
+                                                        $szuletesi_csaladi_nev,
+                                                        $szuletesi_utonev,
+                                                        $szuletesi_hely,
+                                                        $szuletesi_datum,
+                                                        $anyja_szuletesi_csaladi_neve,
+                                                        $anyja_szuletesi_utoneve,            
+                                                        $lakcim_iranyitoszam,
+                                                        $lakcim_varos,
+                                                        $lakcim_utca,
+                                                        $lakcim_hazszam,
+                                                        $telefonszam,
+                                                        $email,            
+                                                        $olvasojegy_azonosito);
             
             if (!$error)
                 $message = 'Az olvasót sikeresen regisztrálta a nyilvántartásban.';
