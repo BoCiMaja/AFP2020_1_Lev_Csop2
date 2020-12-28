f9e9a0ba203653b5c8e4b621c6861f436963313c
JakabZsolt <jakabzsolt001@gmail.com>
2020-12-28 19:54:20 +0100

    librarian validate függvények paraméter átalakítása

--- mvc/app/controllers/librarian.php
+++ mvc/app/controllers/librarian.php
@@ -13,19 +13,49 @@ class Librarian extends Controller {
         if (array_key_exists('submit', $_POST))
         {
             $librarianModel = $this->model('LibrarianModel');
+            $felhasznaloi_nev = trim($_POST['felhasznaloi_nev']); 
+            $jelszo = trim($_POST['jelszo']);
+            $csaladi_nev = trim($_POST['csaladi_nev']);
+            $utonev = trim($_POST['utonev']);
+            $szuletesi_csaladi_nev = trim($_POST['szuletesi_csaladi_nev']);
+            $szuletesi_utonev = trim($_POST['szuletesi_utonev']);
+            $anyja_szuletesi_csaladi_neve = trim($_POST['anyja_szuletesi_csaladi_neve']);
+            $anyja_szuletesi_utoneve = trim($_POST['anyja_szuletesi_utoneve']);
+            $szuletesi_hely = trim($_POST['szuletesi_hely']);
+            $szuletesi_datum = trim($_POST['szuletesi_datum']);
+            $lakcim_iranyitoszam = trim($_POST['lakcim_iranyitoszam']);
+            $lakcim_varos = trim($_POST['lakcim_varos']);
+            $lakcim_utca = trim($_POST['lakcim_utca']);
+            $lakcim_hazszam = trim($_POST['lakcim_hazszam']);
+            $telefonszam = trim($_POST['telefonszam']);
+            $email = trim($_POST['email']);
             
             $error = $librarianModel->validateLibrarianData1(
-                        $_POST['felhasznaloi_nev'], $_POST['jelszo'],
-                        $_POST['szuletesi_csaladi_nev'], $_POST['szuletesi_utonev'], 
-                        $_POST['anyja_szuletesi_csaladi_neve'], $_POST['anyja_szuletesi_utoneve'],
-                        $_POST['szuletesi_hely'], $_POST['szuletesi_datum']);
+                        $olvasojegy_azonosito, $felhasznaloi_nev, $jelszo,
+                        $szuletesi_csaladi_nev, $szuletesi_utonev, 
+                        $anyja_szuletesi_csaladi_neve, $anyja_szuletesi_utoneve,
+                        $szuletesi_hely, $szuletesi_datum);
             
-            $error .= $librarianModel->validateLibrarianData2($_POST['csaladi_nev'], $_POST['utonev'], 
-                        $_POST['lakcim_iranyitoszam'], $_POST['lakcim_varos'], $_POST['lakcim_utca'], $_POST['lakcim_hazszam'],
-                        $_POST['telefonszam'], $_POST['email']);
+            $error .= $librarianModel->validateLibrarianData2($csaladi_nev, $utonev, 
+                        $lakcim_iranyitoszam, $lakcim_varos, $lakcim_utca, $lakcim_hazszam,
+                        $telefonszam, $email);
             
             if (!$error)
-                $error = $librarianModel->registerLibrarian($_POST);
+                $error = $librarianModel->registerLibrarian($felhasznaloi_nev,                                                   $jelszo,
+                                                        $csaladi_nev,
+                                                        $utonev,
+                                                        $szuletesi_csaladi_nev,
+                                                        $szuletesi_utonev,
+                                                        $szuletesi_hely,
+                                                        $szuletesi_datum,
+                                                        $anyja_szuletesi_csaladi_neve,
+                                                        $anyja_szuletesi_utoneve,            
+                                                        $lakcim_iranyitoszam,
+                                                        $lakcim_varos,
+                                                        $lakcim_utca,
+                                                        $lakcim_hazszam,
+                                                        $telefonszam,
+                                                        $email);
             
             if (!$error)
                 $message = 'Az könyvtárost sikeresen regisztrálta a nyilvántartásban.';
